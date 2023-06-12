26b3aa1f696edf950925c97c2c5f1cb881e9c878
singhmanish975310@gmail.com <singhmanish975310@gmail.com>
2023-06-12 06:52:25 +0530

    hello commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Manish Singh')
+})
+
+app.listen(port, () => {
+  console.log('Manish Singh')
+})
